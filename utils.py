c3003a9e3898be1ce30fad2cb3d89865c4331339
sperez8 <karatezeus21@gmail.com>
2017-08-22 09:30:37 -0700

    Added a merged function to join start and duration coordinates of actions

--- utils.py
+++ utils.py
@@ -55,3 +55,51 @@ def get_action_usage(df,column,action):
         A list of tuples with start times of the action and it's duration [(start1,duration1),(start2,duration2),...]
     '''
     return zip(df[df[column].str.contains(action,na=False)]['Time_seconds'],df[df[column].str.contains(action,na=False)]['Duration'])
+
+def merge_action_usage(x,y):
+    '''
+    Given two lists of coordinates, we merged them and return the new coordinates.
+    These coordinates are in the format (start_time, duration)
+    
+    Args:
+        x (list): One set of coordinates
+        y (list): A second set of coordinates
+
+    Returns:
+        A list of tuples with merged start and duration coordinates [(start1,duration1),(start2,duration2),...]
+        
+    For example:
+        x = [(0,1),(2,3),(10,3)] #0,2,3,4,10,11,12
+        y = [(0,2),(3,1),(9,2),(12,2)] #0,1,3,9,10,12,13
+        then merged(x,y) => [(0, 2), (2, 3), (9, 5)] #0,1,2,3,4,9,10,11,12,13
+
+    '''
+    
+    x.extend(y) #we put all the coordinates in one list
+    x.sort() #sort them by start time
+    merged = []
+    
+    #for pairs of coordinates, we check if we can merged them
+    for i,(s1,d1) in enumerate(x):
+        if i != len(x)-1: 
+            s2,d2 = x[i+1] #get next coordinates
+            if s1 == s2: #if same start times, find max duration
+                merged.append((s1,max(d1,d2)))
+                x.remove((s2,d2))
+            elif s1+d1 >= s2+d2: #if one coordinate bounds the other
+                merged.append((s1,d1)) #we add that coordinate
+                x.remove((s2,d2)) #and remove the other
+            elif s1+d1 >= s2: # if they overlap
+                new_duration = d2 + s2-s1 #we calculate a new duration
+                merged.append((s1,new_duration)) #add the new coordinate with earliest start time
+                x.remove((s2,d2)) #and remove the other
+            else:
+                merged.append((s1,d1))
+        else:
+            #these are the last coordinates of x and haven't been merged yet, 
+            # so we try to merge them with the previous coordinates
+            if s1 <= merged[-1][0]+merged[-1][1]:
+                new_start = merged[-1][0]
+                new_duration = d1 + s1 - merged[-1][0]
+                merged[-1] = (new_start,new_duration) #extend the duration of the last coordinate
+    return merged
