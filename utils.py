c78fdc8434bbf6b22d74a7530934eb0084ba3c23
sperez8 <karatezeus21@gmail.com>
2017-08-23 17:58:30 -0700

    Parsing use of range using case data

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, date
-import pandas as pd
+from pandas import notnull
 
 def fix_time(time_start,current_time):
     """This function fixes the timestamps used by converting them to seconds, starting at zero.
@@ -29,7 +29,7 @@ def get_duration(row):
     Returns:
         duration: The difference in time in seconds between the Timeshifted and Time variables.
     """
-    if pd.notnull(row['Timeshifted']): #check that this is not the last action which will have a NA Timeshifted value
+    if notnull(row['Timeshifted']): #check that this is not the last action which will have a NA Timeshifted value
         #check that the time of the next action is indeed later than time of the current actin
         if datetime.combine(date.min, row['Timeshifted']) >= datetime.combine(date.min, row['Time']):
             duration = (datetime.combine(date.min, row['Timeshifted']) - datetime.combine(date.min, row['Time'])).total_seconds()
@@ -43,6 +43,18 @@ def get_duration(row):
 def clean_method(method):
     return method.replace("}","").replace("{","")
     
+def clean_coords(coords_brocken_up):
+    #since the coordinates all subsequent in time, we want to merge them to clean them up.
+    #For example:
+    # coords_brocken_up = [(0,2),(2,5),(7,2)]
+    # coords -> [(0,9)]
+    while True:
+        coords = merge_usage(coords_brocken_up,coords_brocken_up)
+        if coords == coords_brocken_up:
+            return coords
+        else:
+            coords_brocken_up = coords
+
 def get_action_usage(df,column,action):
     '''Given an action or method, we detect its use using a particular column
     and then extract a list of time coordinates for when
@@ -94,14 +106,22 @@ def merge_usage(x,y):
 
     '''
     
-    x.extend(y) #we put all the coordinates in one list
+    x = x+y #we put all the coordinates in one list
+    
+    x = list(set(x)) #remove duplicate coordinates
     x.sort() #sort them by start time
+    
+    if len(x)==1:
+        return x
+    
     merged = []
+    
     
     #for pairs of coordinates, we check if we can merged them
     for i,(s1,d1) in enumerate(x):
         if i != len(x)-1: 
             s2,d2 = x[i+1] #get next coordinates
+#             print s1,d1,s2,d2
             if s1 == s2: #if same start times, find max duration
                 merged.append((s1,max(d1,d2)))
                 x.remove((s2,d2))
@@ -179,10 +199,41 @@ def union_usage(x,y):
 
     return union
 
-def get_merged_method_usage(df, pattern):
+def get_merge_method_usage(df, pattern):
     m1 = get_action_usage_exact(df,'Cleaned method 1',pattern)
     m2 = get_action_usage_exact(df,'Cleaned method 2',pattern)
     return merge_usage(m1,m2)
+
+def get_all_cases(df):
+    '''Given a dataframe with students' activity, we extract all
+    the contrasting cases they were given as well as starting time and
+    length of time for which they were working on that case.
+    
+    Args:
+        df (Pandas dataframe): The dataframe to search in.        
+
+    Returns:
+        coordinates: a dictionary where the keys are the cases and values are time coordinates.
+                    Cases are in the format ('1 2 3 6','2 3 6 7').
+                    Time coordinates are in the format (start_time, duration)
+    '''
+    coordinates = {}
+    
+    #get all possible cases
+    raw_cases = list(set(df['cases']))
+    for raw_case in raw_cases:
+        #clean them up:
+        case = tuple(raw_case.split(','))
+        #we get time coordinates the way we always do
+        coords_brocken_up = get_action_usage(df,'cases',raw_case)
+        coords = clean_coords(coords_brocken_up)
+        #coords should now be a list with 1 item of formt [(start,duration)]
+        if len(coords) == 1:
+            coordinates[case] = coords[0]
+        else:
+            raise ValueError('This case seems to be used more than once: '+case)
+    return coordinates
+
 
 def get_single_value_usage(df):
     method1 = get_action_usage_exact(df,'Cleaned method 1','st1 \d+$')
@@ -195,13 +246,41 @@ REGEX_MEDIAN = 'st1 Median\s?(all)?\s?(Use)?\s?$'
 REGEX_COUNT = 'st1 Count\s?(all)?\s?(Use)?\s?$'
 
 def get_central_tendency_usage(df):
-    average = get_merged_method_usage(df,REGEX_AVERAGE)
-    sumall = get_merged_method_usage(df,REGEX_SUM)
-    median = get_merged_method_usage(df,REGEX_MEDIAN)
-    count = get_merged_method_usage(df,REGEX_COUNT)
+    average = get_merge_method_usage(df,REGEX_AVERAGE)
+    sumall = get_merge_method_usage(df,REGEX_SUM)
+    median = get_merge_method_usage(df,REGEX_MEDIAN)
+    count = get_merge_method_usage(df,REGEX_COUNT)
     merging1 = merge_usage(average,sumall)
     merging2 = merge_usage(median,count)
     return merge_usage(merging1,merging2)
+
+
+def get_regex_range(case_min,case_max):
+    pattern = '^st1 {0} \- {1}\s?$'.format(case_max,case_min)
+    return pattern
+
+def get_range_usage(df):
+    usage = []
+    cases = get_all_cases(df)
+    for case,coords in cases.items():
+        start = coords[0]
+        end = coords[1]
+        #find min and maxes of cases for the regex
+        lmin,lmax = min(case[0].split(" ")),max(case[0].split(" "))
+        rmin,rmax = min(case[1].split(" ")),max(case[1].split(" "))
+        
+        #get all times that the range is used
+        range1 = get_action_usage_exact(df,'Cleaned method 1',get_regex_range(lmin,lmax))
+        range2 = get_action_usage_exact(df,'Cleaned method 2',get_regex_range(rmin,rmax))
+        
+        #keep only the times that fall within the current case
+        range1_for_case = union_usage(range1,[coords])
+        range2_for_case = union_usage(range2,[coords])
+        
+        # Merge when it's used on both cases
+        usage.extend(clean_coords(merge_usage(range1_for_case,range2_for_case)))
+    usage.sort()
+    return usage
 
 def get_evaluation_steps_usage(df):
     submit_usage = get_action_usage(df,"Selection","submit")
@@ -210,5 +289,4 @@ def get_evaluation_steps_usage(df):
     
     ##do some merging
     merged1 = merge_usage(submit_usage,evaluation_usage)
-    merged2 = merge_usage(merged1, checkIntuition_usage)
-    return merged2
+    return merge_usage(merged1, checkIntuition_usage)
